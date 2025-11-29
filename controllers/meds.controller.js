3b743c0e6950e31b256399692c19ba263fdb078f
Andrea <andreaortez11@gmail.com>
2025-11-29 15:45:57 -0600

    changes

--- controllers/meds.controller.js
+++ controllers/meds.controller.js
@@ -2,10 +2,10 @@ const { databaseConnect } = require("../database");
 const { ObjectId } = require('mongodb');
 
 const createMed = async (req, res) => {
-  
+
   let db = await databaseConnect();
   try {
-    
+
     const {
       nombre,
       cantidad,
@@ -16,7 +16,9 @@ const createMed = async (req, res) => {
       id_usuario,
     } = req.body;
 
-    if (!nombre ||!cantidad ||!forma ||!fecha_inicio ||!fecha_fin ||!horas_recordatorio ||!id_usuario) {
+    console.log(req.body)
+
+    if (!nombre || !cantidad || !forma || !fecha_inicio || !fecha_fin || !horas_recordatorio || !id_usuario) {
       return res.status(400).json({ message: 'Faltan campos obligatorios.' });
     }
 
@@ -30,11 +32,11 @@ const createMed = async (req, res) => {
       id_usuario,
 
     };
-    let user = await db.collection('Users').findOne({_id: new ObjectId(id_usuario)});
-    if(!user)   return res.status(400).json({ message: 'Usuario no existe.' });
+    let user = await db.collection('Users').findOne({ _id: new ObjectId(id_usuario) });
+    if (!user) return res.status(400).json({ message: 'Usuario no existe.' });
 
     const result = await db.collection('Medicamentos').insertOne(medicamento);
-    
+
     return res.status(201).json({
       message: 'Medicamento creado',
       medicamento: { _id: result.insertedId, ...medicamento },
@@ -50,7 +52,7 @@ const createMed = async (req, res) => {
 const modifyMed = async (req, res) => {
   let db = await databaseConnect();
   try {
-    
+
     const {
       nombre,
       cantidad,
@@ -62,7 +64,7 @@ const modifyMed = async (req, res) => {
       _id
     } = req.body;
 
-    if ( !_id ||  !nombre ||!cantidad ||!forma ||!fecha_inicio ||!fecha_fin ||!horas_recordatorio) {
+    if (!_id || !nombre || !cantidad || !forma || !fecha_inicio || !fecha_fin || !horas_recordatorio) {
       return res.status(400).json({ message: 'Faltan campos obligatorios.' });
     }
 
@@ -75,24 +77,24 @@ const modifyMed = async (req, res) => {
       horas_recordatorio,
 
     };
-    
+
     const result = await db.collection('Medicamentos').findOneAndUpdate(
-      { _id: new ObjectId(_id) },   
-      { $set: medicamento },        
-      { returnDocument: 'after' }   
+      { _id: new ObjectId(_id) },
+      { $set: medicamento },
+      { returnDocument: 'after' }
     );
     console.log(result)
-    if(!result){
-      res.status(400).json({message: "No se encontró medicamento"})
-    }else{
+    if (!result) {
+      res.status(400).json({ message: "No se encontró medicamento" })
+    } else {
       res.status(200).json({
         message: 'Medicamento modificado',
         medicamento: result,
       });
     }
-    
+
   } catch (error) {
-   
+
     res.status(500).json({ message: 'Error al crear medicamento', details: error.message });
   }
 }
@@ -100,42 +102,42 @@ const modifyMed = async (req, res) => {
 const deleteMed = async (req, res) => {
   const id = req.body._id;
   let db = await databaseConnect();
-  try{
-    if(id){
-      let result =await db.collection("Medicamentos").findOneAndDelete({_id: new ObjectId(id)});
+  try {
+    if (id) {
+      let result = await db.collection("Medicamentos").findOneAndDelete({ _id: new ObjectId(id) });
 
       res.status(200).json({
         message: "Medicamento Borrado exitosamente"
       })
-    }else{
+    } else {
       res.status(400).json({
         message: "No envió un id"
       })
     }
-    
-  }catch(error){
+
+  } catch (error) {
     res.status(500).json({
       message: "Error eliminando el medicamento"
     })
   }
-  
+
 }
 
 const listarMedicamentosActivos = async (req, res) => {
-    try {
-      const id_usuario  = req.params.id;
-      if (id_usuario) {
-        const db = await databaseConnect();
+  try {
+    const id_usuario = req.params.id;
+    if (id_usuario) {
+      const db = await databaseConnect();
 
-        const meds = await db.collection('Medicamentos').find({ id_usuario, tomado: { $exists: false } }).toArray();
-        res.status(200).json({
-          medicamentos: meds,
-          
-        });
-    }else{
+      const meds = await db.collection('Medicamentos').find({ id_usuario, tomado: { $exists: false } }).toArray();
+      res.status(200).json({
+        medicamentos: meds,
+
+      });
+    } else {
       res.status(400).json({
-          message: 'No envió id_usuario',
-        });
+        message: 'No envió id_usuario',
+      });
     }
   } catch (error) {
     console.error(error);
@@ -147,19 +149,19 @@ const listarMedicamentosActivos = async (req, res) => {
 };
 
 const listarMedicamentos = async (req, res) => {
-    try {
-      let id_usuario  = req.params.id;
-      if (id_usuario) {
-        let db = await databaseConnect();
+  try {
+    let id_usuario = req.params.id;
+    if (id_usuario) {
+      let db = await databaseConnect();
 
-        const meds = await db.collection('Medicamentos').find({ id_usuario }).toArray();
-        res.status(200).json({
-          medicamentos: meds,
-        });
-    }else{
+      const meds = await db.collection('Medicamentos').find({ id_usuario }).toArray();
+      res.status(200).json({
+        medicamentos: meds,
+      });
+    } else {
       res.status(400).json({
-          message: 'No envió id_usuario',
-        });
+        message: 'No envió id_usuario',
+      });
     }
   } catch (error) {
     console.error(error);
@@ -170,34 +172,34 @@ const listarMedicamentos = async (req, res) => {
   }
 };
 
-const terminarMedicamento = async(req, res) => {
-    try {
-        const _id = req.params.id;
-        if (_id) {
-          let db = await databaseConnect();
+const terminarMedicamento = async (req, res) => {
+  try {
+    const _id = req.params.id;
+    if (_id) {
+      let db = await databaseConnect();
 
-          const result = await db.collection('Medicamentos').findOneAndUpdate(
-            { _id: new ObjectId(_id) },   
-            { $set: { tomado: true } },        
-            { returnDocument: 'after' }   
-          );
+      const result = await db.collection('Medicamentos').findOneAndUpdate(
+        { _id: new ObjectId(_id) },
+        { $set: { tomado: true } },
+        { returnDocument: 'after' }
+      );
 
-           let resultBorrar =await db.collection("Medicamentos_tomados").findOneAndDelete({id_medicamento: _id});
-          res.status(200).json({
-            medicamentos: result,
-          });
-      }else{
-        res.status(400).json({
-            message: 'No envió id medicamento',
-          });
-      }
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({
-        message: 'Error al obtener medicamentos',
-        details: error.message,
+      let resultBorrar = await db.collection("Medicamentos_tomados").findOneAndDelete({ id_medicamento: _id });
+      res.status(200).json({
+        medicamentos: result,
+      });
+    } else {
+      res.status(400).json({
+        message: 'No envió id medicamento',
       });
     }
+  } catch (error) {
+    console.error(error);
+    return res.status(500).json({
+      message: 'Error al obtener medicamentos',
+      details: error.message,
+    });
+  }
 }
 //recordatorios
 const agregarRecordatorio = async (req, res) => {
@@ -219,7 +221,7 @@ const agregarRecordatorio = async (req, res) => {
 
     return res.status(200).json({
       message: "Recordatorio creado exitosamente",
-      recordatorio: recordatorio 
+      recordatorio: recordatorio
     });
   } catch (error) {
     console.error(error);
@@ -236,46 +238,46 @@ const modificarRecordatorio = async (req, res) => {
     const { _id, proximo_recordatorio, recordatorio_anterior, tomado, notificacion } = req.body;
 
     if (!_id) {
-     res.status(400).json({ message: "No se mandó _id del recordatorio" });
-    }else{
+      res.status(400).json({ message: "No se mandó _id del recordatorio" });
+    } else {
       const update = {};
 
-    if (proximo_recordatorio) {
-      update.proximo_recordatorio = new Date(proximo_recordatorio);
-    }
-    if( recordatorio_anterior){
-      update.recordatorio_anterior= new Date(recordatorio_anterior);
-    }
-    if (tomado) {
-      update.tomado = tomado;
-    }
-    if (notificacion) {
-      update.notificacion = notificacion;
-    }
+      if (proximo_recordatorio) {
+        update.proximo_recordatorio = new Date(proximo_recordatorio);
+      }
+      if (recordatorio_anterior) {
+        update.recordatorio_anterior = new Date(recordatorio_anterior);
+      }
+      if (tomado) {
+        update.tomado = tomado;
+      }
+      if (notificacion) {
+        update.notificacion = notificacion;
+      }
 
-    if (Object.keys(update).length === 0) {
+      if (Object.keys(update).length === 0) {
         return res.status(400).json({ message: "No se mandaron campos para actualizar" });
+      }
+
+      const result = await db.collection("Medicamentos_tomados").findOneAndUpdate(
+        { _id: new ObjectId(_id) },
+        { $set: update },
+        { returnDocument: "after" }
+      );
+
+      if (!result) {
+        res.status(404).json({ message: "Recordatorio no encontrado" });
+      } else {
+        res.status(200).json({
+          message: "Recordatorio modificado exitosamente",
+          recordatorio: result,
+        });
+      }
+
+
     }
 
-    const result = await db.collection("Medicamentos_tomados").findOneAndUpdate(
-      { _id: new ObjectId(_id) },
-      { $set: update },
-      { returnDocument: "after" }
-    );
 
-    if (!result) {
-      res.status(404).json({ message: "Recordatorio no encontrado" });
-    }else{
-      res.status(200).json({
-        message: "Recordatorio modificado exitosamente",
-        recordatorio: result,
-      });
-    }
-
-    
-    }
-
-    
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -287,7 +289,7 @@ const modificarRecordatorio = async (req, res) => {
 
 const buscarRecordatorioPorMedicamento = async (req, res) => {
   try {
-    const med_id = req.params.id; 
+    const med_id = req.params.id;
     if (!med_id) {
       return res.status(400).json({ message: "No se mandó med_id" });
     }
@@ -296,7 +298,7 @@ const buscarRecordatorioPorMedicamento = async (req, res) => {
 
     const recordatorios = await db.collection("Medicamentos_tomados").find({ id_medicamento: med_id }).toArray();
 
-     res.status(200).json({
+    res.status(200).json({
       recordatorios,
     });
   } catch (error) {
@@ -310,11 +312,11 @@ const buscarRecordatorioPorMedicamento = async (req, res) => {
 
 const eliminarRecordatorio = async (req, res) => {
   try {
-    const { _id } = req.body; 
+    const { _id } = req.body;
 
     if (!_id) {
       res.status(400).json({ message: "No se mandó id del recordatorio" });
-    }else{
+    } else {
       const db = await databaseConnect();
 
       const result = await db.collection("Medicamentos_tomados").findOneAndDelete({ _id: new ObjectId(_id) });
@@ -329,7 +331,7 @@ const eliminarRecordatorio = async (req, res) => {
       });
     }
 
-    
+
   } catch (error) {
     console.error(error);
     return res.status(500).json({
@@ -340,16 +342,16 @@ const eliminarRecordatorio = async (req, res) => {
 };
 
 
-module.exports = { 
-    createMed,
-    modifyMed,
-    deleteMed,
-    listarMedicamentos,
-    listarMedicamentosActivos,
-    terminarMedicamento,
-    agregarRecordatorio,
-    modificarRecordatorio,
-    buscarRecordatorioPorMedicamento,
-    eliminarRecordatorio
+module.exports = {
+  createMed,
+  modifyMed,
+  deleteMed,
+  listarMedicamentos,
+  listarMedicamentosActivos,
+  terminarMedicamento,
+  agregarRecordatorio,
+  modificarRecordatorio,
+  buscarRecordatorioPorMedicamento,
+  eliminarRecordatorio
 };
 
