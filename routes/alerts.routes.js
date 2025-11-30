311cb4420d663df6c6778ab856d0db26126f2255
darielsevilla <darielsevilla@gmail.com>
2025-11-29 22:13:19 -0600

    commented endpoints

--- routes/alerts.routes.js
+++ routes/alerts.routes.js
@@ -3,17 +3,17 @@ const router = express.Router();
 const { 
     receiveVitalSignAlert,
     receiveMedicationAlert,
-    getPendingAlerts, 
-    markAlertAsNotified,
-    markAlertAsResolved,
-    getAlertsByElder
+    //getPendingAlerts, 
+    //markAlertAsNotified,
+    //markAlertAsResolved,
+    //getAlertsByElder
 } = require('../controllers/alerts.controller');
 router.post('/receive/vitalsign', receiveVitalSignAlert); 
 router.post('/receive/medication', receiveMedicationAlert); 
-
+/*
 router.get('/pending/:idFamiliar', getPendingAlerts); 
 router.get('/elder/:idElder', getAlertsByElder);      
 router.put('/notified/:idAlert', markAlertAsNotified); 
 router.put('/resolved/:idAlert', markAlertAsResolved); 
-
+*/
 module.exports = router;
